a0cff71b0283cba143d24ea45ceeb5d0d64921dd
gaurav4ever <gauravsharma.mvp@gmail.com>
2019-05-26 13:48:46 +0530

    listing activity added

---
+++ app/src/main/java/com/csapp/bp/bookpurple/activity/ListingActivity.java
@@ -0,0 +1,15 @@
+package com.csapp.bp.bookpurple.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.csapp.bp.bookpurple.R;
+
+public class ListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing);
+    }
+}
