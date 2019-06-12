88e78cebb19673f88defced1ccd4818ef1d384da
gaurav4ever <gauravsharma.mvp@gmail.com>
2019-06-12 20:22:53 +0530

    vendor details activity added

--- app/src/main/java/com/csapp/bp/bookpurple/dagger/component/ModuleComponent.java
+++ app/src/main/java/com/csapp/bp/bookpurple/dagger/component/ModuleComponent.java
@@ -2,6 +2,7 @@ package com.csapp.bp.bookpurple.dagger.component;
 
 import com.csapp.bp.bookpurple.activity.LandingActivity;
 import com.csapp.bp.bookpurple.activity.ListingActivity;
+import com.csapp.bp.bookpurple.activity.VendorDetailsActivity;
 
 /*
  * Written by Gaurav Sharma on 2019-05-19.
@@ -10,4 +11,5 @@ public interface ModuleComponent {
 
     void inject(LandingActivity landingActivity);
     void inject(ListingActivity listingActivity);
+    void inject(VendorDetailsActivity vendorDetailsActivity);
 }
---
+++ app/src/main/java/com/csapp/bp/bookpurple/mvp/interfaces/VendorDetailsViewPresenterContract.java
@@ -0,0 +1,17 @@
+package com.csapp.bp.bookpurple.mvp.interfaces;
+
+import com.csapp.bp.bookpurple.mvp.core.MvpBasePresenterV2;
+
+/*
+ * Written by Gaurav Sharma on 2019-06-12.
+ */
+public interface VendorDetailsViewPresenterContract {
+
+    interface View {
+
+    }
+
+    abstract class Presenter extends MvpBasePresenterV2<View> {
+
+    }
+}
